b1c92b6bbf8e2c625856ea44363e10521bb2f136
JekGum <jack.gumley1@education.nsw.gov.au>
2019-04-04 10:39:52 +1100

    Update gourcerun.bat

--- gourcerun.bat
+++ gourcerun.bat
@@ -1,4 +1,4 @@
 @echo mypath=%cd%
 cd mypath
-gource -f --s 1000 --i 10 --follow-user JekGum --default-user-image JekGum.jpg
+gource -f --s 1000 --i 10 --default-user-image JekGum.jpg --title Software_Major --filename-time 2 --file-idle-time 0
 pause
